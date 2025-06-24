776281528935b80dba5eb4e53822d841fc57af70
JulianBison <bisonjulian@gmail.com>
2025-06-24 17:49:31 -0300

    cambio rutas y agregado nuevo middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,14 +7,14 @@ import {
   removeUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../auth/auth.middleware.js";
-import { verifyRole } from "../auth/roles.middleware.js";
+import { verifyRole , verifyRoleOrOwnership } from "../auth/roles.middleware.js";
 
 const router = Router();
 
 router.get("/", verifyToken, verifyRole( 'superadmin' , 'admin' ), listUsers);
-router.get("/:id",verifyRole("superadmin"), showUser);
+router.get("/:id",verifyRoleOrOwnership("superadmin"), showUser);
 router.post("/",verifyRole("superadmin"), addUser);
-router.put("/:id",verifyRole("superadmin"), editUser);
+router.put("/:id",verifyRoleOrOwnership("superadmin"), editUser);
 router.delete("/:id",verifyRole("superadmin"), removeUser);
 
 export default router;
