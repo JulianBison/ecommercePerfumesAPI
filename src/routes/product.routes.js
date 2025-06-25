268076bd3970ba80258bdb5f5bf714bac25b09ca
JulianBison <bisonjulian@gmail.com>
2025-06-25 16:00:36 -0300

    rutas protegidas por rol

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -7,14 +7,15 @@ import {
   removeProduct,
 } from "../controllers/product.controller.js";
 import { verifyToken } from "../auth/auth.middleware.js";
+import { verifyRole } from "../auth/roles.middleware.js";
 
 const router = Router();
 
 router.get("/", listProducts);
-router.get("/:id", verifyToken, showProduct);
-router.post("/", verifyToken, addProduct);
-router.put("/:id", verifyToken, editProduct);
-router.delete("/:id", verifyToken, removeProduct);
+router.get("/:id", verifyToken,verifyRole("admin","superadmin"), showProduct);
+router.post("/", verifyToken,verifyRole("admin","superadmin"), addProduct);
+router.put("/:id", verifyToken,verifyRole("admin","superadmin"), editProduct);
+router.delete("/:id", verifyToken,verifyRole("admin","superadmin") ,removeProduct);
 
 export default router;
 
